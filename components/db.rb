eab993fa4a4c6dc986f1d78eecd155bb894e60aa
Oleg Moskaliov <41901252+gerslav@users.noreply.github.com>
2019-03-31 13:11:10 +0300

    Update db.rb

--- components/db.rb
+++ components/db.rb
@@ -1,8 +1,8 @@
 def db_connect
-	client = Mysql2::Client.new(
-		:host => "127.0.0.1", 
-		:username => "test_user", 
-		:password => "test_pass", 
-		:database => "test"
+	client = PG::Connection.new(
+		:host => "ec2-54-247-70-127.eu-west-1.compute.amazonaws.com", 
+		:username => "joynujisuykawh", 
+		:password => "ad6f44563030d4ef81260212d1190c167b5ccbbd323d4b7f3541bbfa42d79571", 
+		:database => "d7r3ik4ku140v8"
 	)
 end
